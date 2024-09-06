fc252440cfd8b38c6c2bc6d25dd9030b1ea77643
ekfrhl <78692169+ekfrhl@users.noreply.github.com>
2024-09-06 14:17:44 +0900

    Update 11.c

--- Project3/Project3/11.c
+++ Project3/Project3/11.c
@@ -1,3 +1,13 @@
-#include
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
+int main() {
 
+	int number = 0;
+
+	for (int i = 0; i < 10; i++) {
+		number = rand();
+		printf("number=%d\n", number);
+		return 0;
+	}
