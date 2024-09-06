295f99aa7448cc79939975ee83e6c037f96a5950
ekfrhl <78692169+ekfrhl@users.noreply.github.com>
2024-09-06 17:36:11 +0900

    Update 11.c

--- Project3/Project3/11.c
+++ Project3/Project3/11.c
@@ -52,3 +52,48 @@ int main() {
 	}
 	return 0;
 }
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define SIZE 100 //define을 통한 노가다 방지
+int main() {
+
+	int number[SIZE];
+	int total=0;
+	float average=0.0;
+	int max;
+	int min;
+	
+
+	srand(time(NULL));
+
+	for (int i = 0; i < SIZE; i++) {
+		number[i] = (rand()%100)+1;
+	}
+	
+	for (int i = 0; i < SIZE; i++) {
+		printf("%3d\t", number[i]);
+		if ((i + 1) % 5 == 0)printf("\n");
+	}
+	
+	for (int i = 0; i < SIZE; i++) {
+		total += number[i];
+	}
+	average = (float)total / SIZE;
+	
+	max = number[0];
+	min = number[0];
+	
+	
+	for (int i = 1; i < SIZE; i++) {
+		if (max < number[i])
+			max = number[i];
+		else if (min > number[i])
+			min = number[i];
+	}
+	
+	printf("\naverage=%f\n", average);
+	printf("max=%d\n",max);
+	printf("min=%d\n", min);
+	return 0;
+} 
