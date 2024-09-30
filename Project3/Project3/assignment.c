6332498652f202bfa5469543bb06c23705a40b34
ekfrhl <78692169+ekfrhl@users.noreply.github.com>
2024-09-30 21:35:48 +0900

    Update assignment.c

--- Project3/Project3/assignment.c
+++ Project3/Project3/assignment.c
@@ -635,6 +635,31 @@ int main() {
     } //while 닫는문
     return 0;
 }
+/////////////5주차 3 문자열 7(과제)//////////////////////
+#include <stdio.h>
+
+int main(void) {
+
+	char input[200] = "";
+	int count[26] = { 0 };
+	int ch;
+	int i = 0;
+
+	while ((ch = getchar() != EOF)) {
+		input[i++] = ch;
+	}
+	i = 0;
+	while (input[i] != '\0'){
+		if (input[i] >= 'a' && input[i] <= 'z') {
+			count[input[i] - 'a']++;
+		}
+		i++;
+	}
+	printf("입력문자열: %s\n", input);
+	for (int i = 0; i < 26; i++) 
+		printf("%c 출현횟수: %d\n", 'a' + i, count[i]);
+	return 0;
+}
 
 
 
