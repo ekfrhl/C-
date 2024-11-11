fb00e99fcc55a4dffdab98f2bdb51f051cb07310
ekfrhl <78692169+ekfrhl@users.noreply.github.com>
2024-11-11 20:16:47 +0900

    Update assignment.c

--- Project3/Project3/assignment.c
+++ Project3/Project3/assignment.c
@@ -1446,7 +1446,161 @@ int main() {
 	fclose(fp);
 	return 0;
 }
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#define SIZE 100
+int main() {
 
+	char input[SIZE];
+	FILE* fp = NULL;
+	int i = 0;
+
+	if ((fp = fopen("output.bin", "rb")) == NULL) {
+		printf("error");
+		return 0;
+	}
+	while (!feof(fp)) {
+		fread(&input[i], sizeof(char), 1, fp);
+		i++;
+	}
+	input[--i] = '\0';
+	puts(input);
+
+	fclose(fp);
+	return 0;
+}
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+
+    int array[100];
+    FILE* fp;
+    int i = 0;
+    fp = fopen("array.bin", "rb");
+    if (fp == NULL) return -1;
+
+    while(fread(&array[i++], sizeof(int), 1, fp));
+
+
+
+    i--;
+    for (int j = 0; j < i++; j++) printf("%d ", array[j]);
+    fclose(fp);
+
+    return 0;
+}
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+typedef struct point {
+	int x;
+	int y;
+}POINT;
+
+POINT makePoint(void) {
+	POINT p;
+	scanf_s("%d %d", &p.x, &p.y);
+	return p;
+}
+void makeRandomPoints(POINT p[], int size) {
+	srand(time(NULL));
+	for (int i = 0; i < size; i++) {
+		p[i].x = rand() % 100;
+		p[i].y = rand() % 100;
+	}
+}
+void printPoints(POINT p[], int size) {
+	for (int i = 0; i < size; i++)
+		printf("point[%d]=x:%d,y:%d\n", i, p[i].x, p[i].y);
+}
+void saveFile(POINT p[], int size) {
+	FILE* fp;
+	fp = open("struct.bin", "wb");
+	if (fp == NULL) exit(-1);
+	fwrite(p, sizeof(POINT),size,fp);
+	fclose(fp);
+}
+void loadFile(POINT p[], int size) {
+	FILE* fp;
+	fp = open("struct.bin", "rb");
+	if (fp == NULL) exit(-1);
+	fread(p, sizeof(POINT), size, fp);
+	fclose(fp);
+}
+
+int main() { 
+	POINT p[10];
+	makeRandomPoints(p, 10);
+	loadFile(p, 10);
+	printPoints(p, 10);
+	saveFile(p, 10);
+	return 0;
+} 
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+typedef struct point {
+	int x;
+	int y;
+}POINT;
+
+POINT makePoint(void) {
+	POINT p;
+	scanf_s("%d %d", &p.x, &p.y);
+	return p;
+}
+void makeRandomPoints(POINT p[], int size) {
+	srand(time(NULL));
+	for (int i = 0; i < size; i++) {
+		p[i].x = rand() % 100;
+		p[i].y = rand() % 100;
+	}
+}
+void printPoints(POINT p[], int size) {
+	for (int i = 0; i < size; i++)
+		printf("point[%d]=x:%d,y:%d\n", i, p[i].x, p[i].y);
+}
+void saveFile(POINT p[], int size) {
+	FILE* fp;
+	fp = fopen("struct.bin", "wb");
+	if (fp == NULL) exit(-1);
+	fwrite(p, sizeof(POINT), size, fp);
+	fclose(fp);
+}
+void loadFile(POINT p[], int size) {
+	FILE* fp;
+	fp = fopen("struct.bin", "rb");
+	if (fp == NULL) exit(-1);
+	fread(p, sizeof(POINT), size, fp);
+	fclose(fp);
+}
+
+int main() {
+	FILE* fp;
+	POINT p[10];
+	POINT p2;
+	int pi;
+	loadFile(p, 10);
+	printPoints(p, 10);
+	
+	fp = fopen("struct.bin", "rb");
+	if (fp == NULL)exit(-1);
+	fseek(fp, sizeof(POINT) * 3, SEEK_SET);
+	pi = ftell(fp);
+	printf("pi=%d\n", pi);
+	fread(&p2, sizeof(POINT), 1, fp);
+	printf("p2 -> x:%d, y:%d\n", p2.x, p2.y);
+	fseek(fp,sizeof(POINT)* -2, SEEK_END);
+	printf("pi=%d\n", ftell(fp));
+	fread(&p2, sizeof(POINT), 1, fp);
+	printf("p2 -> x:%d, y:%d\n", p2.x, p2.y);
+	fseek(fp, 0, SEEK_END);
+	printf("size=%ld\n", ftell(fp));
+	fclose(fp);
+	return 0;
+}
 
 
 
